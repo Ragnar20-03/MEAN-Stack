2eef275ec60d44a2000b65767b248146bc71b8e9
Ragnar <roshanpp20@gmail.com>
2023-04-23 20:12:27 +0530

    Custom directive 0

--- CustomDirective/src/app/demo.directive.ts
+++ CustomDirective/src/app/demo.directive.ts
@@ -1,13 +1,26 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appDemo]'
 })
 export class DemoDirective {
 
-  constructor() {
-    console.log("Inside Directive Constructor");
-    
-   }
+  // dependancy injection \
+  // internally angular will automatically perform 
+  // ElementRef eobj=new ElementRef();
+  constructor(public eobj:ElementRef) {
+
+    }
+
+@HostListener('mouseenter')onmouseenter()
+{
+  this.eobj.nativeElement.style.color='orange';
+  
+}
+@HostListener('mouseleave')onmouseleave()
+{
+  this.eobj.nativeElement.style.color='red';
+}
+
 
 }
