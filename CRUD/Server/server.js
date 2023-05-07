81fad1df4c236864df4aa4088ff6fa7c70d7b1e0
Ragnar <roshanpp20@gmail.com>
2023-05-07 19:43:40 +0530

    000

--- CRUD/Server/server.js
+++ CRUD/Server/server.js
@@ -6,8 +6,24 @@ eobj.use(express.json());
 
 eobj.listen(5100 , function(){
     console.log("Server started");
-})
+});
 
 eobj.get('/',function(req,res){
     res.send("Marvellous InfoSystems")
+});
+
+
+batches = [
+    {id : 1,name :"PPA" , duration : "4 Monhs"},
+    {id : 2,name :"Lb" , duration : "3.5 Months"},
+    {id : 3,name :"Python" , duration : "3 Monhs"},
+    {id : 4,name :"LSP" , duration : "4.5 Monhs"}
+];
+
+// Read data from database
+eobj.get('/getbatches' , function(req,res){
+    res.send(batches)
 })
+
+
+eobj.get('/getBatches/:id' , ReadDataID)
